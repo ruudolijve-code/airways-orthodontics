5c31084f75650084af5ba0824ba231d2b7681d1d
ruudtopbags <ruud.olijve@topbags.nl>
2026-08-17 14:23:43 +0200

    Simplify Knowledge module

--- src/Collaboration/Controller/CollaborationController.php
+++ src/Collaboration/Controller/CollaborationController.php
@@ -17,26 +17,25 @@ final class CollaborationController extends AbstractController
             [
                 'name' => 'Ortho Euregio',
                 'logo' => 'images/collaboration/ortho-euregio.svg',
-                'description' => 'Orthodontische expertise binnen de multidisciplinaire beoordeling en behandeling van luchtweg-, groei- en kaakgerelateerde problematiek.',
+                'description' => 'Orthodontische expertise binnen de multidisciplinaire behandeling van kaakontwikkeling, gebitsstand en ademhalingsgerelateerde problematiek.',
                 'professionals' => [
                     [
                         'name' => 'Bart Alink',
                         'function' => 'Tandarts & Orthodontics MSc',
-                        'role' => 'Kaakverbreding en groei van de bovenkaak, 30 jaar ervaring in de behandeling van kinderen en volwassenen met een smalle bovenkaak en ademhalingsproblemen',
+                        'role' => 'Diagnostiek en orthodontische behandeling, met bijzondere expertise in kaakverbreding en de groei van de bovenkaak bij kinderen en volwassenen.',
                         'photo' => 'images/collaboration/professionals/bart-alink.jpg',
-                        
                     ],
                     [
                         'name' => 'Alexander Alink',
                         'function' => 'Tandarts & Orthodontics MSc',
-                        'role' => 'Orthodontische behandeling door kaakverbeding waarmee ademhalingsproblemen en kaakgerelateerde klachten zich kunnen verbeteren',
+                        'role' => 'Orthodontische behandeling gericht op kaakontwikkeling, gebitsstand en, waar geïndiceerd, verbreding van de bovenkaak.',
                         'photo' => 'images/collaboration/professionals/alexander-alink.png',
                         'BIG' => '19933885302',
                     ],
                     [
                         'name' => 'Alexandra Redmer',
                         'function' => 'Orthodontics MSc',
-                        'role' => 'Orthodontische behandeling door kaakverbeding waarmee ademhalingsproblemen en kaakgerelateerde klachten zich kunnen verbeteren',
+                        'role' => 'Orthodontische behandeling gericht op kaakontwikkeling, gebitsstand en, waar geïndiceerd, verbreding van de bovenkaak.',
                         'photo' => 'images/collaboration/professionals/alexandra-redmer.jpg',
                     ],
                 ],
@@ -44,25 +43,38 @@ final class CollaborationController extends AbstractController
             [
                 'name' => 'Kaakmeesterz',
                 'logo' => 'images/collaboration/kaakmeesterz.svg',
-                'description' => 'Specialistische expertise op het gebied van kaakchirurgie, kaakpositie en de relatie tussen anatomie, luchtweg en ademhaling.',
+                'description' => 'Specialistische kaakchirurgische expertise wanneer anatomie of kaakpositie aanleiding geeft voor aanvullende diagnostiek of chirurgische behandeling.',
                 'professionals' => [
                     [
                         'name' => 'Frank Leusink',
                         'function' => 'Kaakchirurg',
-                        'role' => 'Beoordeling van kaakpositie en chirurgische behandelopties',
+                        'role' => 'Beoordeling van kaakpositie en anatomie en, wanneer nodig, uitvoering of advisering van kaakchirurgische behandelopties.',
                         'photo' => 'images/collaboration/professionals/frank-leusink.jpg',
+                    ],
+                ],
+            ],
+            [
+                'name' => 'Daniels Tandheelkunde',
+                'logo' => 'images/collaboration/daniels.svg',
+                'description' => 'Tandheelkundige expertise met bijzondere aandacht voor de relatie tussen mondfunctie, gebit, tongpositie en luchtweg.',
+                'professionals' => [
+                    [
+                        'name' => 'Nurcan Yilmaz',
+                        'function' => 'Reconstructief & restauratief tandarts',
+                        'role' => 'Herkenning en beoordeling van airway-gerelateerde mond- en gebitsproblematiek, verwijzing naar de juiste specialist en uitvoering van kleinere ingrepen zoals een tongriemcorrectie wanneer dit geïndiceerd is.',
+                        'photo' => 'images/collaboration/professionals/nurcan-yilmaz.jpg',
                     ],
                 ],
             ],
             [
                 'name' => 'Ademwinst',
                 'logo' => 'images/collaboration/ademwinst.svg',
-                'description' => 'Expertise op het gebied van functionele ademhaling, ademhalingspatronen en begeleiding naar een efficiëntere neusademhaling.',
+                'description' => 'Functionele begeleiding gericht op neusademhaling, ademhalingspatronen en het ondersteunen van een stabiel behandelresultaat.',
                 'professionals' => [
                     [
                         'name' => 'Steven Zwerink',
                         'function' => 'Ademfysioloog',
-                        'role' => 'Na de behandeling van de kaak en tanden, begeleiding naar een efficiëntere neusademhaling en een verbeterd ademhalingspatroon',
+                        'role' => 'Begeleiding vóór, tijdens en vooral na de orthodontische of kaakchirurgische behandeling naar een efficiëntere neusademhaling en een functioneel ademhalingspatroon.',
                         'photo' => 'images/collaboration/professionals/steven-zwerink.jpg',
                     ],
                 ],
